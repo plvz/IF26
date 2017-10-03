8b7bbb5c8ad30fe382204ad71f6e0369317dafc8
pellevoizin <gpelllevoizin37@gmail.com>
2017-10-04 00:24:47 +0200

    Initial Commit

---
+++ pellevoizin/app/src/main/java/fr/utt/if26/pellevoizin/MainActivity.java
@@ -0,0 +1,16 @@
+package fr.utt.if26.pellevoizin;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.widget.TextView;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        TextView tx = (TextView) findViewById(R.id.message);
+        tx.setText("Message modifié dans le code Java");
+    }
+}
